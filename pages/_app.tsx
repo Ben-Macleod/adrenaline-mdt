6cc1fe850775844f04ce17fe27da82adf3822330
Ben <benstreasurebox@gmail.com>
2022-08-13 22:55:49 -0400

    feat(chakra) expanded the theme and provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/css/index.css';
 import type { AppProps } from 'next/app';
+import { SWRConfig } from 'swr';
+
+// Style Imports
+import '../styles/css/index.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<SWRConfig
+			value={{
+				fetcher: (resource, init) =>
+					fetch(resource, init).then((res) => res.json()),
+			}}
+		>
+			<ChakraProvider resetCSS theme={theme}>
+				<Component {...pageProps} />
+			</ChakraProvider>
+		</SWRConfig>
+	);
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,27 @@
+import { Flex, Input, Spinner, Tag } from '@chakra-ui/react';
+import * as React from 'react';
 import type { NextPage } from 'next';
+import useDebounce from '../lib/hooks/use-debounce';
+import useSWR from 'swr';
 
 const Home: NextPage = () => {
-	return <div>Adrenaline Mdt</div>;
+	const [stringId, setStringId] = React.useState<string>('');
+	const debouncedStringId = useDebounce<typeof stringId>(stringId);
+
+	const { isValidating, data } = useSWR(
+		`/api/community?stringId=${debouncedStringId}`
+	);
+
+	return (
+		<Flex>
+			<Input
+				value={stringId}
+				onChange={(event) => setStringId(event.target.value)}
+			/>
+			{data && data.isValid ? <Tag>Taken</Tag> : <Tag>Not Taken</Tag>}
+			{JSON.stringify(data)}
+		</Flex>
+	);
 };
 
 export default Home;
