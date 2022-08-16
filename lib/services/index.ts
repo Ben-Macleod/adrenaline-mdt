b175631ea126989514d304ecc4d396cd49d9fbeb
Ben Macleod <benstreasurebox@gmail.com>
2022-08-15 23:22:31 -0400

    feat(api/community) added token generations for community

--- lib/services/index.ts
+++ lib/services/index.ts
@@ -1,2 +1,3 @@
-export { default as ErrorService } from './error.service';
-export { default as CommunityService } from './community.service';
+export { default as ErrorService } from "./error.service";
+export { default as CommunityService } from "./community.service";
+export { default as TokenService } from "./token.service";
--- pages/api/community/index.ts
+++ pages/api/community/index.ts
@@ -1,59 +1,55 @@
-import { PrismaClient } from '@prisma/client';
-import { NextApiRequest, NextApiResponse } from 'next';
-import { z } from 'zod';
-import { CommunityService, ErrorService } from '../../../lib/services';
-
-const prisma = new PrismaClient();
+import { NextApiRequest, NextApiResponse } from "next";
+import { z } from "zod";
+import { CommunityService, ErrorService } from "../../../lib/services";
 
 // Endpoint handler.
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-	const method = req.method;
+  const method = req.method;
 
-	// Handle which method the incoming request should be directed too.
-	switch (method) {
-		case 'POST':
-			return POST(req, res);
-		default:
-			console.error('Unknown method reach at [/api/community].');
-			throw new Error('Unknow error reached at [/api/community].');
-	}
+  // Handle which method the incoming request should be directed too.
+  switch (method) {
+    case "POST":
+      return POST(req, res);
+    default:
+      console.error("Unknown method reach at [/api/community].");
+      throw new Error("Unknow method reached at [/api/community].");
+  }
 };
 
 // POST handler.
 // Contains the logic for generating a new community.
 
 const newCommunitySchema = z.object({
-	communityId: z.string().transform((value) => value.toLocaleUpperCase()),
-	name: z.string().transform((value) => value.toLocaleLowerCase()),
+  communityId: z.string().transform((value) => value.toLocaleUpperCase()),
+  name: z.string().transform((value) => value.toLocaleLowerCase()),
 });
 
 const POST = async (req: NextApiRequest, res: NextApiResponse) => {
-	const { communityId, name } = newCommunitySchema.parse(req.body);
+  const { communityId, name } = newCommunitySchema.parse(req.body);
 
-	// Reqiure that name & communityId be passed.
-	if (!communityId && !name) {
-		const { error } = new ErrorService({ internalCode: '2001' });
+  // Reqiure that name & communityId be passed.
+  if (!communityId && !name) {
+    const { error } = new ErrorService({ internalCode: "2001" });
 
-		return res
-			.status(404)
-			.json(
-				error(
-					'Missing parameters for this request, please fix and resubmit.'
-				)
-			);
-	}
+    return res
+      .status(404)
+      .json(error("Missing parameters for this request, please fix and resubmit."));
+  }
 
-	try {
-		const { create } = new CommunityService({});
-		const newCommunity = await create(communityId, name);
+  // Attempt to create new new community.
+  // Response with error if one is caught.
+  try {
+    const { create } = new CommunityService({});
+    const newCommunity = await create(communityId, name);
 
-		res.status(200).json({
-			isSuccess: Boolean(newCommunity),
-			data: { ...newCommunity },
-		});
-	} catch (error) {
-		res.status(500).json({ ...error });
-	}
+    // Respond the JSON of the new comminty.
+    res.status(200).json({
+      isSuccess: Boolean(newCommunity),
+      data: { ...newCommunity },
+    });
+  } catch (error: any) {
+    res.status(500).json({ isSuccess: false, error });
+  }
 };
 
 export default handler;
